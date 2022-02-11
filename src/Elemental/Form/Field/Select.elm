cabc840256776aa626c761221b70090c216cf1f1
Dhruv Dang <dhruv@realfolk.com>
2022-02-11 00:18:16 -0800

    Implement the Select form field.

---
+++ src/Elemental/Form/Field/Select.elm
@@ -0,0 +1,100 @@
+module Elemental.Form.Field.Select exposing
+    ( Flags
+    , Model
+    , Msg
+    , Options
+    , field
+    )
+
+import Elemental.Form.Field as Field
+import Elemental.View.Form.Field.Select as Select
+import Html.Styled as Html exposing (Html)
+
+
+type alias Field value =
+    Field.Field {} {} (Msg_ value) (Options_ value) value
+
+
+field : Field value
+field =
+    Field.build
+        { init = init
+        , update = update
+        , view = view
+        }
+
+
+
+-- MODEL
+
+
+type alias Flags value =
+    Field.Flags {} value
+
+
+type alias Model value =
+    Field.Model {} value
+
+
+init : Flags value -> ( Model value, Cmd (Msg_ value) )
+init flags =
+    ( { value = flags.value
+      , validator = flags.validator
+      , errors = []
+      }
+    , Cmd.none
+    )
+
+
+
+-- UPDATE
+
+
+type alias Msg value =
+    Field.Msg (Msg_ value)
+
+
+type Msg_ value
+    = ChangedInput (Maybe value)
+
+
+update : Msg_ value -> Model value -> ( Model value, Cmd (Msg_ value) )
+update msg model =
+    case msg of
+        ChangedInput Nothing ->
+            ( model, Cmd.none )
+
+        ChangedInput (Just newValue) ->
+            ( { model | value = newValue }, Cmd.none )
+
+
+
+-- VIEW
+
+
+type alias Options value =
+    Field.Options (Msg_ value) (Options_ value)
+
+
+type alias Options_ value =
+    { widgetTheme : Select.Theme
+    , autofocus : Bool
+    , viewCaret : Html (Msg_ value)
+    , choices : List (Select.Choice value)
+    }
+
+
+view : Options value -> Model value -> Html (Msg_ value)
+view options model =
+    Select.view
+        { theme = options.widgetTheme
+        , layout = options.layout
+        , autofocus = options.autofocus
+        , disabled = options.disabled
+        , error = Field.hasError model
+        , onInput = ChangedInput
+        , viewCaret = options.viewCaret
+        , choices = options.choices
+        , customAttrs = []
+        }
+        model.value
