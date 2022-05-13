baa1564720ce65e04571c10319116b0c742bea88
Dhruv Dang <dhruv@realfolk.com>
2022-05-13 11:22:53 -0700

    nix: update flake.lock, add symlinked dependency to elm-lib

--- flake.nix
+++ flake.nix
@@ -9,59 +9,73 @@
     rnixLsp.url = "github:nix-community/rnix-lsp";
     elmPackages.url = "github:realfolk/nix?dir=lib/projects/elm/packages/elm-0.19";
     nodeInterpreter.url = "github:realfolk/nix?dir=lib/projects/node/interpreter/node-17";
+    elmLibSrc = {
+      url = "github:realfolk/elm-lib";
+      flake = false;
+    };
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    flakeUtils,
-    neovim,
-    ranger,
-    rnixLsp,
-    elmPackages,
-    nodeInterpreter,
-    ...
-  }:
+  outputs =
+    { self
+    , nixpkgs
+    , flakeUtils
+    , neovim
+    , ranger
+    , rnixLsp
+    , elmPackages
+    , nodeInterpreter
+    , elmLibSrc
+    , ...
+    }:
     flakeUtils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in
-      {
-        packages = {
-          neovim = neovim.packages.${system}.default;
-          ranger = ranger.packages.${system}.default;
-          node = nodeInterpreter.packages.${system}.default;
-          rnixLsp = rnixLsp.defaultPackage.${system};
-          elm = elmPackages.packages.${system}.elm;
-          elmTest = elmPackages.packages.${system}.elm-test;
-          elmFormat = elmPackages.packages.${system}.elm-format;
-          elmLanguageServer = elmPackages.packages.${system}.elm-language-server;
-        };
+    let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      packages = {
+        neovim = neovim.packages.${system}.default;
+        ranger = ranger.packages.${system}.default;
+        node = nodeInterpreter.packages.${system}.default;
+        rnixLsp = rnixLsp.defaultPackage.${system};
+        elm = elmPackages.packages.${system}.elm;
+        elmTest = elmPackages.packages.${system}.elm-test;
+        elmFormat = elmPackages.packages.${system}.elm-format;
+        elmLanguageServer = elmPackages.packages.${system}.elm-language-server;
+      };
 
-        devShells.default = pkgs.mkShell {
-          buildInputs = builtins.concatLists [
-            (builtins.attrValues self.packages.${system})
-            [
-              pkgs.silver-searcher # ag
-              pkgs.fzf
-              pkgs.openssl
-              pkgs.inotifyTools
-            ]
-          ];
-          shellHook = ''
-            # Load ~/.bashrc if it exists
-            test -f ~/.bashrc && source ~/.bashrc
+      devShells.default = pkgs.mkShell {
+        buildInputs = builtins.concatLists [
+          (builtins.attrValues self.packages.${system})
+          [
+            pkgs.silver-searcher # ag
+            pkgs.fzf
+            pkgs.openssl
+            pkgs.inotifyTools
+          ]
+        ];
+        shellHook = ''
+          # Load ~/.bashrc if it exists
+          test -f ~/.bashrc && source ~/.bashrc
 
-            # Source .env file if present
-            test -f "$PROJECT/.env" && source .env
+          # Source .env file if present
+          test -f "$PROJECT/.env" && source .env
 
-            # Ignore files specified in .gitignore when using fzf
-            # -t only searches text files and includes empty files
-            export FZF_DEFAULT_COMMAND="ag -tl"
+          # Ignore files specified in .gitignore when using fzf
+          # -t only searches text files and includes empty files
+          export FZF_DEFAULT_COMMAND="ag -tl"
 
-            # Initialize $PROJECT environment variable
-            export PROJECT="$PWD"
-          '';
-        };
-      });
+          # Initialize $PROJECT environment variable
+          export PROJECT="$PWD"
+
+          # Symlink GitHub elm dependencies
+          function symlink_dependency {
+            src="$1"
+            dest="$2"
+            mkdir -p "$(dirname $dest)"
+            ln -sfT "$src" "$dest"
+          }
+          symlink_dependency "${elmLibSrc}" "$PROJECT/lib/realfolk/elm-lib"
+        '';
+      };
+    });
 }
