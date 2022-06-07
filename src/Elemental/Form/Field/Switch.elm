57ad164a2220fd9691236ab06c3662cd9f5b54fa
Nduati Kuria <012nkuria@gmail.com>
2022-06-07 14:05:30 +0200

    Add Elemental.Form.Field.Switch and Elemental.View.Form.Field.Switch component

---
+++ src/Elemental/Form/Field/Switch.elm
@@ -0,0 +1,94 @@
+module Elemental.Form.Field.Switch exposing
+    ( Flags
+    , Model
+    , Msg
+    , Msg_
+    , Options
+    , field
+    )
+
+import Elemental.Form.Field as Field
+import Elemental.View.Form.Field.Switch as Switch
+import Html.Styled as H
+
+
+type alias Field =
+    Field.Field {} {} Msg_ Options_ Bool
+
+
+field : Field
+field =
+    Field.build
+        { init = init
+        , update = update
+        , view = view
+        }
+
+
+
+-- MODEL
+
+
+type alias Flags =
+    Field.Flags {} Bool
+
+
+type alias Model =
+    Field.Model {} Bool
+
+
+init : Flags -> ( Model, Cmd Msg_ )
+init flags =
+    ( { value = flags.value
+      , validator = flags.validator
+      , errors = []
+      }
+    , Cmd.none
+    )
+
+
+
+-- UPDATE
+
+
+type alias Msg =
+    Field.Msg Msg_
+
+
+ Msg_
+    = ToggledSwitch Bool
+
+
+update : Msg_ -> Model -> ( Model, Cmd Msg_ )
+update msg model =
+    case msg of
+        ToggledSwitch newValue ->
+            ( { model | value = newValue }, Cmd.none )
+
+
+
+-- VIEW
+
+
+type alias Options =
+    Field.Options Msg_ Options_
+
+
+type alias Options_ =
+    { widgetTheme : Switch.Theme
+    , switchText : String
+    , size : Switch.Size
+    }
+
+
+view : Options -> Model -> H.Html Msg_
+view options model =
+    Switch.view
+        { theme = options.widgetTheme
+        , layout = options.layout
+        , text = options.switchText
+        , disabled = options.disabled
+        , size = options.size
+        , onToggle = ToggledSwitch
+        }
+        model.value
---
+++ src/Elemental/View/Form/Field/Switch.elm
@@ -0,0 +1,321 @@
+module Elemental.View.Form.Field.Switch exposing
+    ( Options
+    , Size(..)
+    , Theme
+    , view
+    )
+
+import Css
+import Css.Transitions
+import Elemental.Css as LibCss
+import Elemental.Layout as L
+import Elemental.Typography as Typography exposing (Typography)
+import Html.Styled as H
+import Html.Styled.Attributes as HA
+import Html.Styled.Events as Events
+
+
+type alias Options msg =
+    { theme : Theme
+    , layout : L.Layout msg
+    , text : String
+    , disabled : Bool
+    , size : Size
+    , onToggle : Bool -> msg
+    }
+
+
+type Size
+    = Small
+    | Medium
+
+
+type alias Theme =
+    { colors :
+        { background :
+            { disabled : Css.Color
+            , off : Css.Color
+            , on : Css.Color
+            }
+        , border :
+            { disabled : Css.Color
+            , off : Css.Color
+            , on : Css.Color
+            }
+        , foreground :
+            { enabled : Css.Color
+            , disabled : Css.Color
+            }
+        , handle :
+            { background :
+                { disabled : Css.Color
+                , off : Css.Color
+                , on : Css.Color
+                }
+            , border :
+                { disabled : Css.Color
+                , off : Css.Color
+                , on : Css.Color
+                }
+            }
+        }
+    , spacerMultiples :
+        { y : Size -> Float
+        , text : Size -> Float
+        }
+    , transitionDuration : Float
+    , typography :
+        { label : Typography
+        }
+    }
+
+
+view : Options msg -> Bool -> H.Html msg
+view options isSelected =
+    let
+        ( width, height ) =
+            case options.size of
+                Small ->
+                    ( 34, 20 )
+
+                Medium ->
+                    ( 42, 24 )
+
+        dimensions =
+            ( width, height )
+
+        attrs =
+            [ HA.css
+                [ Css.width <| Css.px width
+                , Css.height <| Css.px height
+                , Css.position Css.relative
+                ]
+            ]
+    in
+    L.viewColumn
+        L.Normal
+        []
+        [ options.layout.spacerY <|
+            options.theme.spacerMultiples.y options.size
+        , L.viewRow
+            L.Normal
+            []
+            [ H.div attrs
+                [ viewInput options dimensions isSelected
+                , viewHandle options dimensions isSelected
+                ]
+            , options.layout.spacerX <|
+                options.theme.spacerMultiples.text options.size
+            , viewLabel options isSelected
+            ]
+        , options.layout.spacerY <|
+            options.theme.spacerMultiples.y options.size
+        ]
+
+
+viewInput : Options msg -> ( Float, Float ) -> Bool -> H.Html msg
+viewInput options ( width, height ) isSelected =
+    let
+        fieldColors =
+            options.theme.colors
+
+        transitionDuration =
+            options.theme.transitionDuration
+
+        disabledStyle =
+            Css.batch
+                [ Css.backgroundColor fieldColors.background.disabled
+                , LibCss.borderAll fieldColors.border.disabled
+                ]
+
+        normalStyle =
+            Css.batch
+                [ Css.backgroundColor <|
+                    if isSelected then
+                        fieldColors.background.on
+
+                    else
+                        fieldColors.background.off
+                , LibCss.borderAll <|
+                    if isSelected then
+                        fieldColors.border.on
+
+                    else
+                        fieldColors.border.off
+                , Css.Transitions.transition
+                    [ Css.Transitions.left transitionDuration
+                    , Css.Transitions.background transitionDuration
+                    , Css.Transitions.borderColor transitionDuration
+                    ]
+                ]
+
+        interactionStyle =
+            if options.disabled then
+                disabledStyle
+
+            else
+                normalStyle
+
+        css =
+            HA.css
+                [ Css.width <| Css.px width
+                , Css.height <| Css.px height
+                , Css.borderRadius <| Css.px (height / 2)
+                , interactionStyle
+                ]
+
+        baseAttrs =
+            [ HA.type_ "checkbox"
+            , HA.disabled options.disabled
+            , HA.checked isSelected
+            , css
+            ]
+
+        additionalAttrs =
+            if options.disabled then
+                [ HA.css
+                    [ Css.cursor Css.notAllowed
+                    ]
+                ]
+
+            else
+                [ Events.onCheck options.onToggle
+                , HA.css
+                    [ Css.cursor Css.pointer
+                    ]
+                ]
+
+        attrs =
+            baseAttrs ++ additionalAttrs
+    in
+    H.input attrs []
+
+
+viewHandle : Options msg -> ( Float, Float ) -> Bool -> H.Html msg
+viewHandle options ( width, height ) isSelected =
+    let
+        fieldColors =
+            options.theme.colors
+
+        transitionDuration =
+            options.theme.transitionDuration
+
+        handlePadding =
+            3
+
+        size =
+            height - (2 * handlePadding)
+
+        disabledStyle =
+            Css.batch
+                [ Css.backgroundColor fieldColors.handle.background.disabled
+                , LibCss.borderAll fieldColors.handle.border.disabled
+                ]
+
+        normalStyle =
+            Css.batch
+                [ Css.backgroundColor <|
+                    if isSelected then
+                        fieldColors.handle.background.on
+
+                    else
+                        fieldColors.handle.background.off
+                , LibCss.borderAll <|
+                    if isSelected then
+                        fieldColors.handle.border.on
+
+                    else
+                        fieldColors.handle.border.off
+                , Css.Transitions.transition
+                    [ Css.Transitions.left transitionDuration
+                    , Css.Transitions.background transitionDuration
+                    , Css.Transitions.borderColor transitionDuration
+                    ]
+                ]
+
+        css =
+            Css.batch
+                [ Css.width <| Css.px size
+                , Css.height <| Css.px size
+                , Css.borderRadius (Css.px (size / 2))
+                , Css.border2 (Css.px 1) Css.solid
+                , Css.left <|
+                    if isSelected then
+                        Css.px (width - handlePadding - size)
+
+                    else
+                        Css.px handlePadding
+                , Css.top <| Css.px handlePadding
+                , Css.position Css.absolute
+                , Css.pointerEvents Css.none
+                ]
+
+        handleAttr =
+            [ HA.css
+                [ css
+                , if options.disabled then
+                    disabledStyle
+
+                  else
+                    normalStyle
+                ]
+            ]
+    in
+    H.span handleAttr []
+
+
+viewLabel : Options msg -> Bool -> H.Html msg
+viewLabel options currentValue =
+    if String.isEmpty options.text then
+        viewEmptyLabel
+
+    else
+        viewNonEmptyLabel options currentValue
+
+
+viewEmptyLabel : H.Html msg
+viewEmptyLabel =
+    H.text ""
+
+
+viewNonEmptyLabel : Options msg -> Bool -> H.Html msg
+viewNonEmptyLabel options currentValue =
+    let
+        labelColors =
+            options.theme.colors.foreground
+
+        labelTypography =
+            Typography.toStyle options.theme.typography.label
+
+        labelStyle =
+            [ labelTypography
+            , Css.batch <|
+                if options.disabled then
+                    [ Css.cursor Css.notAllowed
+                    , Css.color labelColors.disabled
+                    ]
+
+                else
+                    [ Css.cursor Css.pointer
+                    , Css.color labelColors.enabled
+                    ]
+            ]
+
+        interactionAttr =
+            if options.disabled then
+                []
+
+            else
+                [ Events.onClick <|
+                    options.onToggle (not currentValue)
+                ]
+    in
+    H.div interactionAttr
+        [ L.viewRow
+            L.Normal
+            labelStyle
+            [ options.layout.spacerX <|
+                options.theme.spacerMultiples.text options.size
+            , H.text options.text
+            ]
+        ]
