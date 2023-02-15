61e9c716a5c2299a547b4767fca98db06226766c
Nduati Kuria <012nkuria@gmail.com>
2023-02-15 14:31:24 +0200

    Update implementation

--- src/Elemental/Form/Interaction.elm
+++ src/Elemental/Form/Interaction.elm
@@ -24,160 +24,147 @@ import Json.Decode as JD
 
 
 type Interaction msg
-    = Interaction (Config msg)
+    = Interaction (List (Action msg) -> List (Action msg))
 
 
-type alias Config msg =
-    { onClick : Maybe msg
-    , onDoubleClick : Maybe msg
-    , onMouseEnter : Maybe msg
-    , onMouseLeave : Maybe msg
-    , onMouseOver : Maybe msg
-    , onMouseOut : Maybe msg
-    , onMouseDown : Maybe msg
-    , onMouseUp : Maybe msg
-    , onTouchStart : Maybe msg
-    , onTouchEnd : Maybe msg
-    , onTouchMove : Maybe msg
-    , onFocus : Maybe msg
-    , onBlur : Maybe msg
-    }
-
-
-noneConfig : Config msg
-noneConfig =
-    { onClick = Nothing
-    , onDoubleClick = Nothing
-    , onMouseEnter = Nothing
-    , onMouseLeave = Nothing
-    , onMouseOver = Nothing
-    , onMouseOut = Nothing
-    , onMouseDown = Nothing
-    , onMouseUp = Nothing
-    , onTouchStart = Nothing
-    , onTouchEnd = Nothing
-    , onTouchMove = Nothing
-    , onFocus = Nothing
-    , onBlur = Nothing
-    }
+type Action msg
+    = Click msg
+    | DoubleClick msg
+    | MouseEnter msg
+    | MouseLeave msg
+    | MouseOver msg
+    | MouseOut msg
+    | MouseDown msg
+    | MouseUp msg
+    | TouchStart msg
+    | TouchEnd msg
+    | TouchMove msg
+    | Focus msg
+    | Blur msg
 
 
 none : Interaction msg
 none =
-    Interaction noneConfig
+    Interaction identity
 
 
 onClick : msg -> Interaction msg
-onClick msg =
-    Interaction { noneConfig | onClick = Just msg }
+onClick =
+    Interaction << (::) << Click
 
 
 onDoubleClick : msg -> Interaction msg
-onDoubleClick msg =
-    Interaction { noneConfig | onDoubleClick = Just msg }
+onDoubleClick =
+    Interaction << (::) << DoubleClick
 
 
 onMouseEnter : msg -> Interaction msg
-onMouseEnter msg =
-    Interaction { noneConfig | onMouseEnter = Just msg }
+onMouseEnter =
+    Interaction << (::) << MouseEnter
 
 
 onMouseLeave : msg -> Interaction msg
-onMouseLeave msg =
-    Interaction { noneConfig | onMouseLeave = Just msg }
+onMouseLeave =
+    Interaction << (::) << MouseLeave
 
 
 onMouseOver : msg -> Interaction msg
-onMouseOver msg =
-    Interaction { noneConfig | onMouseOver = Just msg }
+onMouseOver =
+    Interaction << (::) << MouseOver
 
 
 onMouseOut : msg -> Interaction msg
-onMouseOut msg =
-    Interaction { noneConfig | onMouseOut = Just msg }
+onMouseOut =
+    Interaction << (::) << MouseOut
 
 
 onMouseDown : msg -> Interaction msg
-onMouseDown msg =
-    Interaction { noneConfig | onMouseDown = Just msg }
+onMouseDown =
+    Interaction << (::) << MouseDown
 
 
 onMouseUp : msg -> Interaction msg
-onMouseUp msg =
-    Interaction { noneConfig | onMouseUp = Just msg }
+onMouseUp =
+    Interaction << (::) << MouseUp
 
 
 onTouchStart : msg -> Interaction msg
-onTouchStart msg =
-    Interaction { noneConfig | onTouchStart = Just msg }
+onTouchStart =
+    Interaction << (::) << TouchStart
 
 
 onTouchEnd : msg -> Interaction msg
-onTouchEnd msg =
-    Interaction { noneConfig | onTouchEnd = Just msg }
+onTouchEnd =
+    Interaction << (::) << TouchEnd
 
 
 onTouchMove : msg -> Interaction msg
-onTouchMove msg =
-    Interaction { noneConfig | onTouchMove = Just msg }
+onTouchMove =
+    Interaction << (::) << TouchMove
 
 
 onFocus : msg -> Interaction msg
-onFocus msg =
-    Interaction { noneConfig | onFocus = Just msg }
+onFocus =
+    Interaction << (::) << Focus
 
 
 onBlur : msg -> Interaction msg
-onBlur msg =
-    Interaction { noneConfig | onBlur = Just msg }
+onBlur =
+    Interaction << (::) << Blur
 
 
 and : Interaction msg -> Interaction msg -> Interaction msg
-and (Interaction interaction2) (Interaction interaction1) =
-    let
-        overrideIfSet fn =
-            case fn interaction2 of
-                Just i ->
-                    Just i
-
-                Nothing ->
-                    fn interaction1
-    in
-    Interaction
-        { onClick = overrideIfSet .onClick
-        , onDoubleClick = overrideIfSet .onDoubleClick
-        , onMouseEnter = overrideIfSet .onMouseEnter
-        , onMouseLeave = overrideIfSet .onMouseLeave
-        , onMouseOver = overrideIfSet .onMouseOver
-        , onMouseOut = overrideIfSet .onMouseOut
-        , onMouseDown = overrideIfSet .onMouseDown
-        , onMouseUp = overrideIfSet .onMouseUp
-        , onTouchStart = overrideIfSet .onTouchStart
-        , onTouchEnd = overrideIfSet .onTouchEnd
-        , onTouchMove = overrideIfSet .onTouchMove
-        , onFocus = overrideIfSet .onFocus
-        , onBlur = overrideIfSet .onBlur
-        }
+and (Interaction b) (Interaction a) =
+    Interaction (a >> b)
 
 
 toAttrs : Interaction msg -> List (H.Attribute msg)
-toAttrs (Interaction interaction) =
+toAttrs (Interaction toActions) =
+    List.reverse <| List.map toEvent (toActions [])
+
+
+toEvent : Action msg -> H.Attribute msg
+toEvent action =
     let
         on name =
             JD.succeed >> HE.on name
     in
-    [ Maybe.map HE.onClick interaction.onClick
-    , Maybe.map HE.onDoubleClick interaction.onDoubleClick
-    , Maybe.map HE.onMouseEnter interaction.onMouseEnter
-    , Maybe.map HE.onMouseLeave interaction.onMouseLeave
-    , Maybe.map HE.onMouseOver interaction.onMouseOver
-    , Maybe.map HE.onMouseOut interaction.onMouseOut
-    , Maybe.map HE.onMouseDown interaction.onMouseDown
-    , Maybe.map HE.onMouseUp interaction.onMouseUp
-    , Maybe.map (on "touchstart") interaction.onTouchStart
-    , Maybe.map (on "touchend") interaction.onTouchEnd
-    , Maybe.map (on "touchmove") interaction.onTouchMove
-    , Maybe.map HE.onFocus interaction.onFocus
-    , Maybe.map HE.onBlur interaction.onBlur
-    ]
-        |> List.filterMap identity
+    case action of
+        Click msg ->
+            HE.onClick msg
+
+        DoubleClick msg ->
+            HE.onDoubleClick msg
+
+        MouseEnter msg ->
+            HE.onMouseEnter msg
+
+        MouseLeave msg ->
+            HE.onMouseLeave msg
+
+        MouseOver msg ->
+            HE.onMouseOver msg
+
+        MouseOut msg ->
+            HE.onMouseOut msg
+
+        MouseDown msg ->
+            HE.onMouseDown msg
+
+        MouseUp msg ->
+            HE.onMouseUp msg
+
+        TouchStart msg ->
+            on "touchstart" msg
+
+        TouchEnd msg ->
+            on "touchend" msg
+
+        TouchMove msg ->
+            on "touchmove" msg
+
+        Focus msg ->
+            HE.onFocus msg
+
+        Blur msg ->
+            HE.onBlur msg
