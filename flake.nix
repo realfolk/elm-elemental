a9736c05842528c74c9586bde26d7c67c10b8f67
Dwayne Crooks <me@dwaynecrooks.com>
2023-11-08 12:00:01 -0400

    Refactor flake.nix and safely upgrade the Elm dependencies

--- flake.nix
+++ flake.nix
@@ -1,17 +1,21 @@
 {
-  description = "Packages and shells for developing Real Folk's Elemental project.";
+  description = "A development shell for developing Real Folk's Elemental project.";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=23.05";
     flakeUtils.url = "github:numtide/flake-utils";
-    neovim.url = "github:realfolk/nix?dir=lib/packages/neovim";
-    ranger.url = "github:realfolk/nix?dir=lib/packages/ranger";
-    rnixLsp.url = "github:nix-community/rnix-lsp";
-    elmPackages.url = "github:realfolk/nix?dir=lib/projects/elm/packages/elm-0.19";
-    nodeInterpreter.url = "github:realfolk/nix?dir=lib/projects/node/interpreter/node-17";
-    elmLibSrc = {
-      url = "github:realfolk/elm-lib/83a65a2b9c8cd95aa4c5ef4d9532ec44bf0b0be3";
-      flake = false;
+
+    realfolkNix = {
+      url = "github:realfolk/nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flakeUtils.follows = "flakeUtils";
+    };
+
+    elmLib = {
+      url = "github:realfolk/elm-lib";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flakeUtils.follows = "flakeUtils";
+      inputs.realfolkNix.follows = "realfolkNix";
     };
   };
 
@@ -19,43 +23,50 @@
     { self
     , nixpkgs
     , flakeUtils
-    , neovim
-    , ranger
-    , rnixLsp
-    , elmPackages
-    , nodeInterpreter
-    , elmLibSrc
+    , realfolkNix
+    , elmLib
     , ...
     }:
     flakeUtils.lib.eachDefaultSystem (system:
     let
       pkgs = nixpkgs.legacyPackages.${system};
+      realfolkNixPkgs = realfolkNix.packages.${system};
+
+      neovim = realfolkNixPkgs.neovim;
+      nodejs = realfolkNixPkgs.nodejs;
+      ranger = realfolkNixPkgs.ranger;
+      rnixLsp = realfolkNixPkgs.rnixLsp;
+
+      realfolkNixLib = realfolkNix.lib.${system};
+      elmPackages = realfolkNixLib.elmPackages;
     in
     {
-      packages = {
-        neovim = neovim.packages.${system}.default;
-        ranger = ranger.packages.${system}.default;
-        node = nodeInterpreter.packages.${system}.default;
-        rnixLsp = rnixLsp.defaultPackage.${system};
-        elm = elmPackages.packages.${system}.elm;
-        elmTest = elmPackages.packages.${system}.elm-test;
-        elmFormat = elmPackages.packages.${system}.elm-format;
-        elmLanguageServer = elmPackages.packages.${system}.elm-language-server;
-      };
-
       devShells.default = pkgs.mkShell {
-        buildInputs = builtins.concatLists [
-          (builtins.attrValues self.packages.${system})
-          [
-            pkgs.silver-searcher # ag
-            pkgs.fzf
-            pkgs.openssl
-            pkgs.inotifyTools
-          ]
+        packages = [
+          # Third Party
+          neovim
+          nodejs
+          pkgs.fzf
+          pkgs.inotifyTools
+          pkgs.openssl
+          pkgs.silver-searcher
+          ranger
+          rnixLsp
+
+          # Elm
+          elmPackages.elm
+          elmPackages.elm-format
+          elmPackages.elm-json
+          elmPackages.elm-language-server
+          elmPackages.elm-test
         ];
+
         shellHook = ''
           # Load ~/.bashrc if it exists
           test -f ~/.bashrc && source ~/.bashrc
+
+          # Initialize environment variables
+          export PROJECT="$PWD"
 
           # Source .env file if present
           test -f "$PROJECT/.env" && source .env
@@ -64,17 +75,14 @@
           # -t only searches text files and includes empty files
           export FZF_DEFAULT_COMMAND="ag -tl"
 
-          # Initialize $PROJECT environment variable
-          export PROJECT="$PWD"
-
-          # Symlink GitHub elm dependencies
+          # Symlink GitHub Elm dependencies
           function symlink_dependency {
             src="$1"
             dest="$2"
             mkdir -p "$(dirname $dest)"
             ln -sfT "$src" "$dest"
           }
-          symlink_dependency "${elmLibSrc}" "$PROJECT/lib/realfolk/elm-lib"
+          symlink_dependency "${elmLib}" "$PROJECT/lib/realfolk/elm-lib"
         '';
       };
     });
